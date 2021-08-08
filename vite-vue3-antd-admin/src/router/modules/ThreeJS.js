9833e48a353e42d3b244aed33caa7899652b5862
dlAm <917988385@qq.com>
2021-08-08 21:05:19 +0800

    feat: threejs

---
+++ vite-vue3-antd-admin/src/router/modules/ThreeJS.js
@@ -0,0 +1,10 @@
+export default [
+    {
+      path: '/ThreeJS/neteaseCloud',
+      name: 'NeteaseCloud',
+      meta: {
+        title: '网易云',
+      },
+      component: () => import('../../view/API/NeteaseCloud/NeteaseCloud.vue'),
+    }
+  ]
