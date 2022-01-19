5f1611750c864ce0d53e09fabeb2004d10a03e5d
陈罗林 <chenluolin@100cbc.com>
2022-01-19 14:30:47 +0800

    m

--- vite-vue3-antd-admin/src/view/typescript/base/index.vue
+++ vite-vue3-antd-admin/src/view/typescript/base/index.vue
@@ -6,11 +6,19 @@
 </template>
 
 <script>
-import { defineComponent } from "@vue/runtime-core";
+import { defineComponent, onMounted } from "@vue/runtime-core";
 
 export default defineComponent({
   setup () {
-    let a = '1'
+    onMounted(() => {
+      // 主页面监听message事件,接收子组件的值
+      window.addEventListener('message', function (e) {
+        console.log(e.data) // 打印值
+      }, false)
+      setTimeout(() => {
+        window.frames[0].postMessage('123', 'http://127.0.0.1:3000/typescript/base.html');
+      }, 10000)
+    })
   }
 })
 </script>
